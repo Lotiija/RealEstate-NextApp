233797f27655ec52e7b1edd9d13b34f93d570b21
Lotiija <milchyloti@gmial.com>
2024-06-18 22:06:04 +0100

    added .env file

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import HomeProperties from '@/components/HomeProperty';
 import InfoBoxes from '@/components/infoBoxes';
 
 const HomePage = () => {
+  console.log(process.env.MONGODB_URI)
+
   return (
     <>
       <Hero />
