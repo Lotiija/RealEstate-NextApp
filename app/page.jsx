e3c95860159ad00b5b9bf3014f8e6b54dcbaa67b
Lotiija <milchyloti@gmial.com>
2024-06-19 00:14:05 +0100

    created api route

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import Hero from '@/components/Hero';
 import HomeProperties from '@/components/HomeProperty';
 import InfoBoxes from '@/components/infoBoxes';
-import connectDB from '@/config/database';
 
-const HomePage = async () => {
-  await connectDB();
+const HomePage = () => {
 
   return (
     <>
