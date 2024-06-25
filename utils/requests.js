63d914ff3bdf6d2b1d8103f29d4cb9560a4b924a
Lotiija <milchyloti@gmial.com>
2024-06-25 02:51:28 +0100

    Authentication process

--- utils/requests.js
+++ utils/requests.js
@@ -1,6 +1,7 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN ||
 null;
 
+// Fetch all properties
 async function fetchProperties() {
   try {
     // Handle the case where the domain is not available yet
@@ -22,4 +23,26 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// fetch single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case where the domain is not available yet
+    // This piece of code id for deployment.
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/api/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
